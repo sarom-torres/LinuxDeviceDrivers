c39a0ec9ac41e3d29fda89eeb26d1cb00d56b948
sarom-torres <saromtorres@gmail.com>
2020-02-14 09:08:43 -0300

    Cap3 - Corrigindo dev_t

--- cap3/scull.c
+++ cap3/scull.c
@@ -2,7 +2,10 @@
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/fs.h> //usada para as funções do numero do modulo
+#include <linux/cdev.h> //usada para alocar e registrar estruturas do tipo struct cdev
+
 #include "scull.h"
+
 
 //SCULL_MAJOR está definida em scull.h
 int scull_major = SCULL_MAJOR;
@@ -10,21 +13,44 @@ int scull_major = SCULL_MAJOR;
 int scull_minor = 0;
 int scull_nr_devs = SCULL_NR_DEVS;
 //int scull_nr_devs = 4;
-dev_t dev = 0;
+
+//dev_t dev = 0; //##global tmp pq  precisava usar no registro. Está incorreto?
+
+struct scull_dev * scull_devices; //alocado no __scull_init
+
 
 MODULE_LICENSE("DuaL BSD/GPL");
 
-// struct file_operations scull_fops = {
-//     ponteiro para a propria estrutura, impede que o módulo seja descarregado enquanto está em uso
-//     .owner = THIS_MODULE, 
-//     usado para mudar a posição atual de leitura/escrita no file
-//     .llseek = scull_llseek,
-//     
-// };
+/*
+struct file_operations scull_fops = {
+    //ponteiro para a propria estrutura, impede que o módulo seja descarregado enquanto está em uso
+    .owner = THIS_MODULE, 
+    //usado para mudar a posição atual de leitura/escrita no file
+    .llseek = scull_llseek,
+    .read = scull_read,
+    .write = scull_write,
+    //para emitir comandos específicos do device
+    .ioctl = scull_ioctl,
+    .open = scull_open,
+    .release = scull_release,
+};
+
+// Configurando a estrutura char_dev para esse device
+//## Não deveria ser um cdev no argumento ao inves de um scull_dev
+static void scull_setup_cdev(struct scull_dev *dev, int index){
+    int err, devno = MKDEV(scull_major, scull_minor + index); //##Pq fazer o MKDEV novamente?
+    
+    //cdev representa o char device internamente
+    // inicializando a estrutura cdev. 
+    cdev_init(&dev->cdev, &scull_fops);
+    
+    
+}*/
 
 static int __init scull_init(void){
 	int result;
-
+    dev_t dev = 0;
+    
 	if (scull_major){
 		dev = MKDEV(scull_major,scull_minor);
 		result = register_chrdev_region(dev,scull_nr_devs,"scull");
@@ -50,9 +76,11 @@ static int __init scull_init(void){
 }
 
 static void __exit scull_exit(void){
+    
+    dev_t devno = MKDEV(scull_major, scull_minor);
 	
 	//cancela o registro do major number
-	unregister_chrdev_region(dev,scull_nr_devs);
+	unregister_chrdev_region(devno,scull_nr_devs);
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
