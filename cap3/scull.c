34960876cadb0f0983c485a5d67d89ce91ccc3d5
sarom-torres <saromtorres@gmail.com>
2020-02-17 22:16:13 -0300

    Cap3 - Erro no scull_trim

--- cap3/scull.c
+++ cap3/scull.c
@@ -3,20 +3,21 @@
 #include <linux/types.h>
 #include <linux/fs.h> //usada para as funções do numero do modulo
 #include <linux/cdev.h> //usada para alocar e registrar estruturas do tipo struct cdev
+#include <linux/slab.h> //usada para o gerenciamento de memória
+#include <linux/kernel.h> //contém a macro container_of
+#include <linux/fcntl.h> //contém as f_flags
 
 #include "scull.h"
 
 
-//SCULL_MAJOR está definida em scull.h
 int scull_major = SCULL_MAJOR;
-//int scull_major = 0;
 int scull_minor = 0;
 int scull_nr_devs = SCULL_NR_DEVS;
-//int scull_nr_devs = 4;
+int scull_quantum = SCULL_QUANTUM;
+int scull_qset = SCULL_QSET;
 
-//dev_t dev = 0; //##global tmp pq  precisava usar no registro. Está incorreto?
 
-struct scull_dev * scull_devices; //alocado no __scull_init
+struct scull_dev *scull_devices; //representação interna do device | alocado no __scull_init 
 
 
 MODULE_LICENSE("DuaL BSD/GPL");
@@ -29,11 +30,36 @@ struct file_operations scull_fops = {
     .llseek = scull_llseek,
     .read = scull_read,
     .write = scull_write,
-    //para emitir comandos específicos do device
-    .ioctl = scull_ioctl,
     .open = scull_open,
     .release = scull_release,
 };
+
+//Libera toda a area de dados do scull_dev
+int scull_trim(struct scull_dev *dev){
+    
+    struct scull_qset *next, *dptr;
+    int qset = dev->qset;
+    int i;
+    
+    //percorrendo cada scull_qset do scull_dev
+    for(dptr=dev->data; dptr; dptr=next){
+        
+        if(dptr->data){
+            for(i=0;i < qset;i++)
+                kfree(dptr->data[i]);
+            kfree(dptr->data);
+            dptr->data = NULL;
+        }
+        next = dptr->next;
+        kfree(dptr);
+    }
+    
+    dev->size = 0;
+    dev->quantum = scull_quantum;
+    dev->qset = scull_qset;
+    dev->data = NULL;
+    return 0;
+}
 
 // Configurando a estrutura char_dev para esse device
 //## Não deveria ser um cdev no argumento ao inves de um scull_dev
@@ -44,7 +70,7 @@ static void scull_setup_cdev(struct scull_dev *dev, int index){
     // inicializando a estrutura cdev. 
     cdev_init(&dev->cdev, &scull_fops);
     dev->cdev.owner = THIS_MODULE;
-    dev->cdev.pos = &scull_fops;
+    dev->cdev.ops = &scull_fops;
     
     //informa ao kernel o device
     err = cdev_add(&dev->cdev,devno,1); //arg: (estrutura cdev, numero do device, quantia de devices que podem ser associados a ele)
@@ -54,8 +80,55 @@ static void scull_setup_cdev(struct scull_dev *dev, int index){
     
 }
 
+ssize_t scull_read(struct file *filp, char __user *buf,size_t count, loff_t *f_pos);
+
+ssize_t scull_write(struct file *filp, const char __user *buf,size_t count, loff_t *f_pos);
+
+loff_t scull_llseek(struct file *filp, loff_t off, int whence);
+
+long scull_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+
+//ver se devo manter no .h
+
+/*
+ *scull_open faz a inicialização e preparação para operações posteriores
+ *inode:  possui as informações no campo i_cdev que contem a struct cdev coonfigurada.
+ */
+int scull_open(struct inode *inode, struct file *filp){
+    //informações do device
+    struct scull_dev *dev; 
+    
+    /*Macro container_of usada para obtero endereço de uma estrutura 
+     *que contém um campo do qual apenas conhecemos seu endereço
+     *www.embarcados.com.br/conhecendo-a-macro-container_of/
+     *container_of(ponteiro,container_type,container_field)
+     */
+    dev = container_of(inode->i_cdev,struct scull_dev,cdev);
+    
+    //armazena um ponteiro para scull_dev no campo private_data da estrutura file
+    filp->private_data = dev;
+    
+    if((filp->f_flags & O_ACCMODE) == O_WRONLY){
+        //libera toda area de dados quando o file é aberto para leitura
+        scull_trim(dev);
+    }
+    return 0;
+}
+int scull_release(struct inode *inode, struct file *filp){
+    return 0;
+}
+
+static void __exit scull_exit(void){
+    
+    dev_t devno = MKDEV(scull_major, scull_minor);
+	
+	//cancela o registro do major number
+	unregister_chrdev_region(devno,scull_nr_devs);
+	printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+
 static int __init scull_init(void){
-	int result;
+	int result,i;
     dev_t dev = 0;
     
 	if (scull_major){
@@ -78,19 +151,36 @@ static int __init scull_init(void){
 		return result;
 	}
 
-// FAZER A ALOCAÇÃO DO SCULL_DEVICES
+    //alocando memória
+    // o retorno da função é um ponteiro para a area de memória alocada
+    scull_devices = kmalloc(scull_nr_devs*sizeof(struct scull_dev),GFP_KERNEL);
+    
+    //Caso não tenha alocado memória
+    if(!scull_devices){
+        result = -ENOMEM;
+        goto fail;        
+    }
+    
+    //função que preenche um bloco de memória com determinado valor
+    //args: (ponteiro para o bloco de memoria, valor a ser escrito,numero de bytes)
+    memset(scull_devices,0,scull_nr_devs*sizeof(struct scull_dev));
+    
+    for(i=0;i<scull_nr_devs;i++){
+        scull_devices[i].quantum = scull_quantum;
+        scull_devices[i].qset = scull_qset;
+        sema_init(&scull_devices[i].sem,1);
+        scull_setup_cdev(&scull_devices[i],i);
+    }
+    
+    
+    fail:
+        scull_exit();
+        return result;
 	
 	return 0;
 }
 
-static void __exit scull_exit(void){
-    
-    dev_t devno = MKDEV(scull_major, scull_minor);
-	
-	//cancela o registro do major number
-	unregister_chrdev_region(devno,scull_nr_devs);
-	printk(KERN_ALERT "Goodbye, cruel world\n");
-}
+
 
 module_init(scull_init);
 module_exit(scull_exit);
--- cap3/scull.h
+++ cap3/scull.h
@@ -1,15 +1,63 @@
 
-//-----------------------Macros
+//----------------------- | Macros
+//Major number
 #ifndef SCULL_MAJOR
 #define SCULL_MAJOR 0
 #endif
 
-
+//Números de devices
 #ifndef SCULL_NR_DEVS
 #define SCULL_NR_DEVS 4
 #endif
 
-//----------------------Variáveis públicas
+//Bytes da area de memoria
+#ifndef SCULL_QUANTUM
+#define SCULL_QUANTUM 4000
+#endif
+
+//Tamanho do array de quantum
+#ifndef SCULL_QSET
+#define SCULL_QSET 1000
+#endif
+
+//-------------------------- | Structs
+
+//Representação dos quantum sets
+struct scull_qset{
+    void **data;
+    struct scull_qset *next;
+};
+
+struct scull_dev{
+    struct scull_qset *data; //ponteiro para o primeiro quantum sets
+    int quantum; //tamanho atual do quantum
+    int qset; //tamanho atual do array
+    unsigned long size; //Quantia de dados armazenados
+    unsigned int access_key;  //??
+    struct semaphore sem;
+    struct cdev cdev;
+};
+
+//---------------------- | Variáveis públicas
 //foram declaradas em outro arquivo do código
 extern int scull_major;
 extern int scull_nr_devs;
+extern int scull_quantum;
+extern int scull_qset;
+
+
+//----------------------- | Protótipos das funções
+
+//int scull_p_init(dev_t dev);
+int scull_trim(struct scull_dev *dev);
+
+ssize_t scull_read(struct file *filp, char __user *buf,size_t count, loff_t *f_pos);
+
+ssize_t scull_write(struct file *filp, const char __user *buf,size_t count, loff_t *f_pos);
+
+loff_t scull_llseek(struct file *filp, loff_t off, int whence);
+
+//ver se devo manter no .h
+int scull_open(struct inode *inode, struct file *filp);
+int scull_release(struct inode *inode, struct file *filp);
+
